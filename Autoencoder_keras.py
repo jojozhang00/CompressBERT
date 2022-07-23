6452e5616d26fbc0eff486a188d7c8db790821fd
Gaifan Zhang <95146616+jojozhang00@users.noreply.github.com>
2022-07-23 09:43:04 +0100

    updated

--- Autoencoder_keras.py
+++ Autoencoder_keras.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import numpy as np
 import os
+import csv
 import tensorflow as tf
 from tensorflow import keras
 from keras import layers, losses
@@ -32,17 +33,22 @@ sts_train = pd.read_table(
     skip_blank_lines=True,
     usecols=[4, 5, 6],
     names=["sim", "sent_1", "sent_2"])
+sts_test = pd.read_table(
+    os.path.join(os.path.dirname(sts_dataset), "stsbenchmark", "sts-test.csv"),
+    error_bad_lines=False,
+    quoting=csv.QUOTE_NONE,
+    skip_blank_lines=True,
+    usecols=[4, 5, 6],
+    names=["sim", "sent_1", "sent_2"])
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("device for training: ", device)
 
+# apply sentence embeddings
+model = SentenceTransformer('sentence-transformers/stsb-bert-base')
+
 text1 = sts_train['sent_1'].tolist()
 text2 = sts_train['sent_2'].tolist()
-
-# apply sentence embeddings
-# model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
-model = SentenceTransformer('sentence-transformers/stsb-bert-base')
-# stsb-bert-base work better
 embeddings1 = model.encode(text1)
 embeddings2 = model.encode(text2)
 
@@ -50,25 +56,34 @@ similarity = torch.cosine_similarity(torch.tensor(embeddings1), torch.tensor(emb
 result = spearmanr(similarity.detach().numpy(), sts_train['sim'])
 print("The spearmanr result for sentence-transformers is:", result.correlation)
 
+# for test data
+t1 = sts_test['sent_1'].tolist()
+t2 = sts_test['sent_2'].tolist()
+e1 = model.encode(t1) 
+e2 = model.encode(t2) 
+
+# # concat train and test sets (transductive learning)
+# sts_all = pd.concat([sts_train, sts_test], axis = 0, ignore_index = True)
+# text1_all = sts_all['sent_1'].tolist()
+# text2_all = sts_all['sent_2'].tolist()
+# embeddings1_all = model.encode(text1_all) 
+# embeddings2_all = model.encode(text2_all) 
 
 print('Apply autoencoder')
+
 # apply autoencoder based method
-# dataLoader is used to load the dataset 
-# for training
-percent = np.arange(0.1, 1.1, 0.1).tolist()
-dim_ls = []
-for index in range(len(percent)):
-    dim_ls.append(int(768*percent[index]))
-# dim_ls = np.arange(300, 800, 50).tolist()
-# dim_ls.append(768)
+dim_ls = np.arange(50, 370, 20).tolist()
+new_ls = [int(x/2) for x in dim_ls]
 result_AE_ls = []
 
 # stack
 for latent_dim in dim_ls:
 
     input = Input(shape=(768,))
-    encoded = layers.Dense(latent_dim, activation='relu')(input)
-    decoded = layers.Dense(768, activation='sigmoid')(encoded)
+    encoded = layers.Dense(latent_dim*2, activation='relu')(input)
+    encoded = layers.Dense(latent_dim, activation='relu')(encoded)
+    decoded = layers.Dense(latent_dim*2, activation='relu')(encoded)
+    decoded = layers.Dense(768, activation='sigmoid')(decoded)
 
     # model initialization
     # construct autoencoder
@@ -79,18 +94,20 @@ for latent_dim in dim_ls:
 
     autoencoder.compile(optimizer='adam', loss=losses.MeanSquaredError())
     autoencoder.fit(embeddings1, embeddings1,
-                    epochs = 100,
+                    epochs = 70,
                     shuffle = False,
                     validation_data = (embeddings2, embeddings2))
 
-    embeddings1_encoded = encoder.predict(embeddings1)
-    embeddings2_encoded = encoder.predict(embeddings2)
+    embeddings1_encoded = encoder.predict(e1)
+    embeddings2_encoded = encoder.predict(e2)
 
     similarity = torch.cosine_similarity(torch.tensor(embeddings1_encoded), torch.tensor(embeddings2_encoded), dim=1) 
-    result_AE = spearmanr(similarity.detach().numpy(), sts_train['sim'])
+    result_AE = spearmanr(similarity.detach().numpy(), sts_test['sim'])
     result_AE_ls.append(result_AE.correlation)
 
     print (f'Taking {latent_dim} as the hidden layer dimension for AE, the spearmanr result is:', result_AE.correlation)
+
+print(result_AE_ls)
 
 # plot line chart
 plt.figure(1)
@@ -100,51 +117,3 @@ plt.title('spearmanr results for auto-encoder')
 plt.xlabel('hidden layer dimension')
 plt.ylabel('spearman r')
 plt.savefig('./AE_tf.jpg')
-
-
-
-
-
-
-
-
-
-
-'''
-
-class Autoencoder(tf.keras.Model):
-  def __init__(self, latent_dim):
-    super(Autoencoder, self).__init__()
-    self.latent_dim = latent_dim   
-
-    self.encoder = tf.keras.Sequential([
-      layers.Dense(latent_dim, activation='relu'),
-    ])
-
-    self.decoder = tf.keras.Sequential([
-      layers.Dense(768, activation='sigmoid'),
-    ])
-
-  def call(self, x):
-    encoded = self.encoder(x)
-    decoded = self.decoder(encoded)
-    return encoded, decoded
-
-# model initialization
-autoencoder = Autoencoder(latent_dim)
-
-autoencoder.compile(optimizer='adam', loss=losses.MeanSquaredError())
-
-autoencoder.fit(embeddings1, embeddings1,
-                epochs=50,
-                shuffle=False,
-                validation_data=(embeddings2, embeddings2))
-
-# save model
-model.save("./model_tfAE.h5")
-# load model
-model = load_model('./model_tfAE.h5')
-# summarize model
-model.summary()
-
-'''
