8435eb92de4e08f2cb753c332f7c096c19a6556e
Gaifan Zhang <95146616+jojozhang00@users.noreply.github.com>
2022-07-24 09:19:30 +0100

    updated

--- SVD.py
+++ SVD.py
@@ -20,8 +20,6 @@ torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 
 
-
-
 # import STS-B dataset 
 sts_dataset = tf.keras.utils.get_file(
     fname="Stsbenchmark.tar.gz",
@@ -44,102 +42,95 @@ sts_test = pd.read_table(
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("device for training: ", device)
 
+# apply sentence embeddings
+model = SentenceTransformer('sentence-transformers/stsb-bert-base')
+
 text1 = sts_train['sent_1'].tolist()
 text2 = sts_train['sent_2'].tolist()
+embeddings1 = model.encode(text1)
+embeddings2 = model.encode(text2)
 
-# apply sentence embeddings
-# model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
-model = SentenceTransformer('sentence-transformers/stsb-bert-base')
-# stsb-bert-base work better
-
-# get the embeddings
-def get_embeddings(text):
-    embeddings = model.encode(text)
-    # convert numpy array to tensor
-    embeddings = torch.tensor(embeddings)
-    return embeddings
-
-embeddings1 = get_embeddings(text1) 
-embeddings2 = get_embeddings(text2) 
-
-similarity = torch.cosine_similarity(embeddings1, embeddings2, dim=1) 
+similarity = torch.cosine_similarity(torch.tensor(embeddings1), torch.tensor(embeddings2), dim=1) 
 result = spearmanr(similarity.detach().numpy(), sts_train['sim'])
-print("For train set:")
-print("Spearman r for sentence-transformers is:", result.correlation)
+print("The spearmanr result for sentence-transformers is:", result.correlation)
 
-
-# define a method for SVD
-# select top biggest (count = eigenvalues * percentage) eigenvalues
-def k_svd(embeddings, percent):
-    U, Sigma, VT = np.linalg.svd(embeddings)   
-    # Sigma only contains the values on the diagonal
-    count = int(Sigma.shape[0]*percent)
-    U = U[:, :count]
-    Sigma = Sigma[:count]
-    Sigma = np.diag(Sigma)
-    VT = VT[:count,:]  
-    re_embeddings = np.dot(np.dot(U, Sigma), VT)
-    return re_embeddings
-
-percent = np.arange(0.1, 1.1, 0.1).tolist()
-
-# for train set
-result_SVD = []
-
-print('Apply SVD:')
-
-for index in range(len(percent)):
-    embeddings1_SVD = k_svd(embeddings1, percent[index])
-    embeddings2_SVD = k_svd(embeddings2, percent[index])
-
-    embeddings1_SVD = torch.tensor(embeddings1_SVD)
-    embeddings2_SVD = torch.tensor(embeddings2_SVD)
-
-    similarity_SVD = torch.cosine_similarity(embeddings1_SVD, embeddings2_SVD, dim=1) 
-    result = spearmanr(similarity_SVD.detach().numpy(), sts_train['sim'])
-    result_SVD.append(result.correlation)
-
-    print ('Taking {:.2%}'.format(percent[index]), 'of the eigenvalues, spearman r is:', result.correlation)
-
+# for test data
+t1 = sts_test['sent_1'].tolist()
+t2 = sts_test['sent_2'].tolist()
+e1 = model.encode(t1) 
+e2 = model.encode(t2) 
 
 # concat train and test sets (transductive learning)
 sts_all = pd.concat([sts_train, sts_test], axis = 0, ignore_index = True)
-
 text1_all = sts_all['sent_1'].tolist()
-text2_all = sts_all['sent_2'].tolist()
+embeddings1_all = model.encode(text1_all) 
 
-embeddings1_all = get_embeddings(text1_all) 
-embeddings2_all = get_embeddings(text2_all) 
+# define a method for SVD
+def k_svd(data, dim, test):
+    U, Sigma, VT = np.linalg.svd(data)   
+    # Sigma only contains the values on the diagonal
+    # U = U[:, :count]
+    # Sigma = Sigma[:count]
+    # Sigma = np.diag(Sigma)
+    VT = VT[:dim,:]  
+    redata = np.dot(test, VT.T)
+    return redata
 
-similarity = torch.cosine_similarity(embeddings1_all, embeddings2_all, dim=1) 
-result = spearmanr(similarity.detach().numpy(), sts_all['sim'])
-print("For train and test set:")
-print("Spearman r for sentence-transformers is:", result.correlation)
+dim_ls = np.arange(10, 100, 10).tolist()
+result_ls_aa = []
+result_ls_ae = []
+result_ls_aee = []
 
+# M_train(train)
+print('M_train(train):')
+for index in range(len(dim_ls)):
+    reduced_1 = k_svd(embeddings1, dim_ls[index], embeddings1)
+    reduced_2 = k_svd(embeddings1, dim_ls[index], embeddings2)
+    print(reduced_2.shape)
 
-# for train and test set
-result_SVD_all = []
+    similarity_SVD = torch.cosine_similarity(torch.tensor(reduced_1), torch.tensor(reduced_2), dim=1) 
+    result = spearmanr(similarity_SVD.detach().numpy(), sts_train['sim'])
+    result_ls_aa.append(result.correlation)
 
-for index in range(len(percent)):
-    embeddings1_SVD = k_svd(embeddings1_all, percent[index]) # take the top biggest eigenvalues
-    embeddings2_SVD = k_svd(embeddings2_all, percent[index])
+    print('Taking {}'.format(dim_ls[index]), 'as the projection dim, spearman r is:', result.correlation)
 
-    embeddings1_SVD = torch.tensor(embeddings1_SVD)
-    embeddings2_SVD = torch.tensor(embeddings2_SVD)
+# M_train(test)
+print('M_train(test):')
+for index in range(len(dim_ls)):
+    reduced_1 = k_svd(embeddings1, dim_ls[index], e1)
+    reduced_2 = k_svd(embeddings1, dim_ls[index], e2)
+    print(reduced_2.shape)
 
-    similarity_SVD = torch.cosine_similarity(embeddings1_SVD, embeddings2_SVD, dim=1) 
-    result = spearmanr(similarity_SVD.detach().numpy(), sts_all['sim'])
-    result_SVD_all.append(result.correlation)
+    similarity_SVD = torch.cosine_similarity(torch.tensor(reduced_1), torch.tensor(reduced_2), dim=1) 
+    result = spearmanr(similarity_SVD.detach().numpy(), sts_test['sim'])
+    result_ls_ae.append(result.correlation)
 
-    print ('Taking {:.2%}'.format(percent[index]), 'of the eigenvalues, spearman r is:', result.correlation)
+    print('Taking {}'.format(dim_ls[index]), 'as the projection dim, spearman r is:', result.correlation)    
+
+# M_{train+test}(test)
+print('M_train(test):')
+for index in range(len(dim_ls)):
+    reduced_1 = k_svd(embeddings1_all, dim_ls[index], e1)
+    reduced_2 = k_svd(embeddings1_all, dim_ls[index], e2)
+    print(reduced_2.shape)
+
+    similarity_SVD = torch.cosine_similarity(torch.tensor(reduced_1), torch.tensor(reduced_2), dim=1) 
+    result = spearmanr(similarity_SVD.detach().numpy(), sts_test['sim'])
+    result_ls_aee.append(result.correlation)
+
+    print('Taking {}'.format(dim_ls[index]), 'as the projection dim, spearman r is:', result.correlation)    
+
 
 # plot line chart
-plt.scatter(percent, result_SVD)
-plt.plot(percent, result_SVD, label='train set')
-plt.scatter(percent, result_SVD_all)
-plt.plot(percent, result_SVD_all, label = 'train and test set')
+plt.scatter(dim_ls, result_ls_aa)
+plt.plot(dim_ls, result_ls_aa, label='M_train(train)')
+plt.scatter(dim_ls, result_ls_ae)
+plt.plot(dim_ls, result_ls_ae, label = 'M_train(test)')
+plt.scatter(dim_ls, result_ls_aee)
+plt.plot(dim_ls, result_ls_aee, label = 'M_{train+test}(test)')
 plt.title('spearman r for SVD')
-plt.xlabel('percentage')
+plt.xlabel('dimension')
 plt.ylabel('spearman r')
 plt.legend()
 plt.savefig('./SVD.jpg')
+
