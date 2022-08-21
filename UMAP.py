258f10e571b50d76d98bec0b6ff4364bfb6c2684
Gaifan Zhang <95146616+jojozhang00@users.noreply.github.com>
2022-08-21 11:10:55 +0100

    updated

--- UMAP.py
+++ UMAP.py
@@ -19,7 +19,6 @@ torch.cuda.manual_seed_all(0)
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 
-
 # # import STS-B dataset 
 sts_dataset = tf.keras.utils.get_file(
     fname="Stsbenchmark.tar.gz",
@@ -42,17 +41,13 @@ sts_test = pd.read_table(
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("device for training: ", device)
 
-# apply sentence embeddings
 model = SentenceTransformer('sentence-transformers/stsb-bert-base')
 
 text1 = sts_train['sent_1'].tolist()
 text2 = sts_train['sent_2'].tolist()
-embeddings1 = model.encode(text1)
-embeddings2 = model.encode(text2)
-
-similarity = torch.cosine_similarity(torch.tensor(embeddings1), torch.tensor(embeddings2), dim=1) 
-result = spearmanr(similarity.detach().numpy(), sts_train['sim'])
-print("The spearmanr result for sentence-transformers is:", result.correlation)
+# embeddings1 = model.encode(text1)
+# embeddings2 = model.encode(text2)
+embeddings = model.encode(text1+text2)
 
 # for test data
 t1 = sts_test['sent_1'].tolist()
@@ -60,23 +55,29 @@ t2 = sts_test['sent_2'].tolist()
 e1 = model.encode(t1) 
 e2 = model.encode(t2) 
 
-# concat train and test sets (transductive learning)
-sts_all = pd.concat([sts_train, sts_test], axis = 0, ignore_index = True)
-text1_all = sts_all['sent_1'].tolist()
-embeddings1_all = model.encode(text1_all) 
+similarity = torch.cosine_similarity(torch.tensor(e1), torch.tensor(e2), dim=1) 
+result_na = spearmanr(similarity.detach().numpy(), sts_test['sim'])
+print("The spearmanr r for sentence-transformers is:", result_na.correlation)
 
-print("Apply UMAP:")
+# concat train and test sets (transductive learning)
+# sts_all = pd.concat([sts_train, sts_test], axis = 0, ignore_index = True)
+# text1_all = sts_all['sent_1'].tolist()
+# embeddings1_all = model.encode(text1_all) 
 
 result_ls = []
-new_dim_ls = np.arange(2, 100, 10).tolist()
+new_dim_ls = np.arange(15, 16, 1).tolist()
 
 for index in range(len(new_dim_ls)):
 
     new_dim = int(new_dim_ls[index])
     print(">> new dim: {}".format(new_dim))
 
-    reducer = umap.UMAP(n_components=new_dim).fit(embeddings1)
-    # reducer = umap.UMAP(n_components=new_dim).fit(embeddings1_all)
+    reducer = umap.UMAP(n_neighbors=50,
+                        n_components=new_dim,
+                        min_dist=0.00,
+                        metric='cosine',
+                        init='random',
+                        random_state=0).fit(embeddings)
 
     reduced_1 = reducer.transform(e1)
     reduced_2 = reducer.transform(e2)
